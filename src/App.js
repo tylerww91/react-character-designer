ac1c5b503f5c8727175b928c682858ef5a21c06d
Tyler Watson <tylerww91@gmail.com>
2023-01-11 17:43:52 -0800

    builds Header component and basic css styling in Header.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Layout/Header/Header';
+// import Footer from './components/Layout/Footer/Footer';
+// import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* <Home />
+      <Footer /> */}
     </div>
   );
+}
+{
+  /* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header> */
 }
 
 export default App;
