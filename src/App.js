bb0ad38adfb5fb042d060d602bf0921c55ba243f
Tyler Watson <tylerww91@gmail.com>
2023-01-13 11:11:06 -0800

    builds boilerplate for Home.js, adds background image and minor css styling

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import './App.css';
 
 import Header from './components/Layout/Header/Header';
 import Footer from './components/Layout/Footer/Footer';
-// import Home from './components/Home/Home';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Home /> */}
+      <Home />
       <Footer />
     </div>
   );
 }
-{
-  /* <header className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <p>
-    Edit <code>src/App.js</code> and save to reload.
-  </p>
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn React
-  </a>
-</header> */
-}
+// {
+//   /* <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header> */
+// }
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+import React from 'react';
+import background from '../../final-destination.jpg';
+
+// import Preview from '../Preview/Preview';
+// import Editor from '../Editor/Editor';
+
+import './Home.css';
+
+export default function Home() {
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {/* <Preview />
+      <Editor /> */}
+    </main>
+  );
+}
