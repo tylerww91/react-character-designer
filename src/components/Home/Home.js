e2dcff7fa07520e095ce53eaaf1267cce315b2d0
Tyler Watson <tylerww91@gmail.com>
2023-01-13 12:05:52 -0800

    builds boilerplate for Preview component, adds hard coded images for css styling purposes, adds css styling to Preview.css and Home.css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,16 +2,16 @@
 import React from 'react';
 import background from '../../final-destination.jpg';
 
-// import Preview from '../Preview/Preview';
-// import Editor from '../Editor/Editor';
+import Preview from '../Preview/Preview';
+import Editor from '../Editor/Editor';
 
 import './Home.css';
 
 export default function Home() {
   return (
     <main style={{ backgroundImage: `url(${background})` }}>
-      {/* <Preview />
-      <Editor /> */}
+      <Editor />
+      <Preview />
     </main>
   );
 }
---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Preview.css';
+
+export default function Preview() {
+  return (
+    <>
+      <div className="preview">
+        <img className="head" src="../../cage-head.png" />
+        <img className="torso" src="../../green-torso.png" />
+        <img className="legs" src="../../elf-legs.png" />
+      </div>
+    </>
+  );
+}
