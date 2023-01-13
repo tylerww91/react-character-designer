fe2eee00c66b45a8475864148b9ba7de3a4f103d
Tyler Watson <tylerww91@gmail.com>
2023-01-13 11:35:41 -0800

    builds boilerplate for Editor.js and adds character designer form

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './Editor.css';
+
+export default function Editor() {
+  return (
+    <div className="editor">
+      <div className="form-control">
+        <input name="name" type="text" />
+        <label htmlFor="name">Name</label>
+      </div>
+      <div className="form-control">
+        <select name="head">
+          <option value="rock">Rock</option>
+          <option value="cage">Cage</option>
+          <option value="frank">Frank</option>
+        </select>
+        <label>Head</label>
+      </div>
+      <div className="from-control">
+        <select name="torso">
+          <option value="green">Green</option>
+          <option value="yellow">Yellow</option>
+          <option value="blue">Blue</option>
+        </select>
+        <label>Torso</label>
+      </div>
+      <div className="form-control">
+        <select name="legs">
+          <option value="muscular">Muscular</option>
+          <option value="feminine">Feminine</option>
+          <option value="elf">Elf</option>
+        </select>
+        <label>Legs</label>
+      </div>
+      <div className="form-controls">
+        <textarea name="catchphrase" />
+        <label>Catchphrase</label>
+      </div>
+    </div>
+  );
+}
